0a9bb0b20baf9ea28decb253a381741897b889bb
orenzhang <orenzhang@ovinc.cn>
2024-11-01 18:02:32 +0800

    chore(build): shrink image size

--- scripts/pylint.sh
+++ scripts/pylint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pylint --disable=C0114,W0613,C0115,W1113,W0223,C0116,R0903 --max-line-length=120 $(git ls-files '*.py')
+pylint --disable=C0114,W0613,C0115,W1113,W0223,C0116,R0903,R0901,W0236 --max-line-length=120 $(git ls-files '*.py')
